f6e73354f5fd9dd837b37993b8a6beda6a0d6916
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2021-11-08 22:22:51 +0300

    активация формы после отрисовки карты

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ import {makeLoadErrorMessage} from './popup.js';
 import {debounce} from './util.js';
 import './map.js';
 import './filter.js';
+import {activateForm, deactivateForm} from './switch.js';
 import {getData} from './api.js';
 import {filterAndShow, onFilterChange, onFilterReset} from './filter.js';
 
 setFormSubmit(resetForm);
 
 getData((data) => {
+  deactivateForm();
   filterAndShow(data);
+  activateForm();
   onFilterChange(debounce(
     () => filterAndShow(data),
   ));
