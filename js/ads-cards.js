5ba81eca0cb93b75e1b791bead56efb8c5c4a2e7
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2021-11-14 15:34:57 +0300

    Д25

--- js/ads-cards.js
+++ js/ads-cards.js
@@ -9,10 +9,47 @@ const HOUSE_TYPE = {
 const similarAdsTemplate = document.querySelector('#card').content;
 const popup = similarAdsTemplate.querySelector('.popup');
 
-const getAdvertisementMarks = (dataArray) => {
-  const adsClone = popup.cloneNode(true);
+const getFeatures = (dataArray, adsClone) => {
+  const adsFeatures = dataArray.offer.features;
   const featuresContainer = adsClone.querySelector('.popup__features');
   const featureList = featuresContainer.querySelectorAll('.popup__feature');
+
+  if (!adsFeatures) {
+    return featuresContainer.remove();
+  } else {
+    featureList.forEach((featureListItem) => {
+      const isChecked = adsFeatures.some(
+        (adsFeature) => featureListItem.classList.contains(`popup__feature--${  adsFeature}`),
+      );
+      if (!isChecked) {
+        featureListItem.remove();
+      }
+    });
+  }
+};
+
+const getPhotos = (dataArray, adsClone) => {
+  const photoContainer = adsClone.querySelector('.popup__photos');
+  const adsPhotos = dataArray.offer.photos;
+
+  if (!adsPhotos) {
+    return photoContainer.remove();
+  } else {
+    photoContainer.innerHTML = '';
+
+    adsPhotos.forEach((photoItem) => {
+      const img = document.createElement('img');
+      img.classList.add('popup__photo');
+      img.src = photoItem;
+      img.height = 40;
+      img.width = 45;
+      photoContainer.appendChild(img);
+    });
+  }
+};
+
+const getAdvertisementMarks = (dataArray) => {
+  const adsClone = popup.cloneNode(true);
 
   adsClone.querySelector('.popup__title').textContent = dataArray.offer.title;
   adsClone.querySelector('.popup__text--address').textContent = dataArray.offer.address;
@@ -22,52 +59,14 @@ const getAdvertisementMarks = (dataArray) => {
   adsClone.querySelector('.popup__text--time').textContent = `Заезд после ${ dataArray.offer.checkin}, выезд до ${ dataArray.offer.checkout}`;
   adsClone.querySelector('.popup__avatar').src = dataArray.author.avatar;
 
-  const getFeatures = () => {
-    const adsFeatures = dataArray.offer.features;
-
-    if (!adsFeatures) {
-      return featuresContainer.remove();
-    } else {
-      featureList.forEach((featureListItem) => {
-        const isChecked = adsFeatures.some(
-          (adsFeature) => featureListItem.classList.contains(`popup__feature--${  adsFeature}`),
-        );
-        if (!isChecked) {
-          featureListItem.remove();
-        }
-      });
-    }
-  };
-
-  getFeatures();
-
   if (!dataArray.offer.description) {
     adsClone.querySelector('.popup__description').classList.add('visually-hidden');
   } else {
     adsClone.querySelector('.popup__description').textContent = dataArray.offer.description;
   }
 
-  const getPhotos = () => {
-    const adsPhotos = dataArray.offer.photos;
-    const photoContainer = adsClone.querySelector('.popup__photos');
-
-    if (!adsPhotos) {
-      return photoContainer.remove();
-    } else {
-      photoContainer.innerHTML = '';
-
-      dataArray.offer.photos.forEach((photoItem) => {
-        const img = document.createElement('img');
-        img.classList.add('popup__photo');
-        img.src = photoItem;
-        img.height = 40;
-        img.width = 45;
-        photoContainer.appendChild(img);
-      });
-    }
-  };
-
-  getPhotos();
+  getFeatures(dataArray, adsClone);
+  getPhotos(dataArray, adsClone);
 
   return adsClone;
 };
