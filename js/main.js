35f2bbe1098d35a59c121806f74729396a9eb7f0
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2021-11-14 14:46:36 +0300

    Б1 - Оптимизация активации формы / фильтров - исправление 2

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ import {makeLoadErrorMessage} from './popup.js';
 import {debounce} from './util.js';
 import './map.js';
 import './filter.js';
-import {activateFilters,deactivateForm} from './switch.js';
+import {activateFilters} from './switch.js';
 import {getData} from './api.js';
 import {filterAndShow, onFilterChange, onFilterReset} from './filter.js';
 import './avatar.js';
 
 getData((data) => {
-  deactivateForm();
+
   filterAndShow(data);
   activateFilters();
   onFilterChange(debounce(
